9b0bfdc22a94b1c84dc122daa70d57b0086860f4
nishi633 <citron.nn633@gmail,com>
2023-11-05 23:04:41 +0900

    Select answer numbers

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,3 +1,21 @@
+class HitAndBlow {
+  answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  answer: string[] = []
+  tryCount = 0
+
+  setting() {
+    const answerLength = 3
+
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length)
+      const selectedItem = this.answerSource[randNum]
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem)
+      }
+    }
+  }
+}
+
 const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? '\n' : ''))
 }
@@ -15,4 +33,6 @@ const promptInput = async (text: string) => {
   const age = await promptInput('年齢を入力してください')
   console.log(age)
   process.exit()
+  const hitAndBlow = new HitAndBlow()
+  hitAndBlow.setting()
 })()
