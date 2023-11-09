6b945b33f5612c66149562555fbffbf7703b4022
nishi633 <citron.nn633@gmail,com>
2023-11-09 22:08:56 +0900

    Add game mode

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -2,6 +2,11 @@ class HitAndBlow {
   private readonly answerSource = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   private answer: string[] = []
   private tryCount = 0
+  private mode: 'normal' | 'hard'
+
+  constructor(mode: 'normal' | 'hard') {
+    this.mode = mode
+  }
 
   setting() {
     const answerLength = 3
@@ -79,7 +84,7 @@ const promptInput = async (text: string) => {
 }
 
 ;(async () => {
-  const hitAndBlow = new HitAndBlow()
+  const hitAndBlow = new HitAndBlow('normal')
   hitAndBlow.setting()
   await hitAndBlow.play()
   hitAndBlow.end()
