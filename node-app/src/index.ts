79103d3a8922247a3d2da7c40a4f7216dec7effd
nishi633 <citron.nn633@gmail,com>
2023-11-13 21:46:48 +0900

    Copy janken

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -152,6 +152,90 @@ const promptInput = async (text: string) => {
   return readLine()
 }
 
+// jankenメソッドはコピー
+// https://github.com/awesome-typescript-book/code-snapshot/blob/40033d1060933f3f98e645f7dc1fb4a8e2928969/03_node-app/013/05_%E3%82%B2%E3%83%BC%E3%83%A0%E3%81%AE%E9%81%B8%E6%8A%9E%E6%A9%9F%E8%83%BD%E3%81%AE%E5%AE%9F%E8%A3%852/src/index.ts#L157-L236
+const jankenOptions = ['rock', 'paper', 'scissors'] as const
+type JankenOption = typeof jankenOptions[number]
+
+class Janken {
+  private rounds = 0
+  private currentRound = 1
+  private result = {
+    win: 0,
+    lose: 0,
+    draw: 0,
+  }
+
+  async setting() {
+    const rounds = Number(await promptInput('何本勝負にしますか？'))
+    if (Number.isInteger(rounds) && 0 < rounds) {
+      this.rounds = rounds
+    } else {
+      await this.setting()
+    }
+  }
+
+  async play() {
+    const userSelected = await promptSelect(`【${this.currentRound}回戦】選択肢を入力してください。`, jankenOptions)
+    const randomSelected = jankenOptions[Math.floor(Math.random() * 3)]
+    const result = Janken.judge(userSelected, randomSelected)
+    let resultText: string
+
+    switch (result) {
+      case 'win':
+        this.result.win += 1
+        resultText = '勝ち'
+        break
+      case 'lose':
+        this.result.lose += 1
+        resultText = '負け'
+        break
+      case 'draw':
+        this.result.draw += 1
+        resultText = 'あいこ'
+        break
+    }
+    printLine(`---\nあなた: ${userSelected}\n相手${randomSelected}\n${resultText}\n---`)
+
+    if (this.currentRound < this.rounds) {
+      this.currentRound += 1
+      await this.play()
+    }
+  }
+
+  end() {
+    printLine(`\n${this.result.win}勝${this.result.lose}敗${this.result.draw}引き分けでした。`)
+    this.reset()
+  }
+
+  private reset() {
+    this.rounds = 0
+    this.currentRound = 1
+    this.result = {
+      win: 0,
+      lose: 0,
+      draw: 0,
+    }
+  }
+
+  static judge(userSelected: JankenOption, randomSelected: JankenOption) {
+    if (userSelected === 'rock') {
+      if (randomSelected === 'rock') return 'draw'
+      if (randomSelected === 'paper') return 'lose'
+      return 'win'
+    } else if (userSelected === 'paper') {
+      if (randomSelected === 'rock') return 'win'
+      if (randomSelected === 'paper') return 'draw'
+      return 'lose'
+    } else {
+      if (randomSelected === 'rock') return 'lose'
+      if (randomSelected === 'paper') return 'win'
+      return 'draw'
+    }
+  }
+}
+
+
 ;(async () => {
   new GameProcedure().start()
 })()
