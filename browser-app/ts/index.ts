0b7479b8353cbd93528158373e93cda490c7d5ad
nishi633 <citron.nn633@gmail,com>
2023-11-21 23:12:51 +0900

    Add EventListener

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,3 +1,27 @@
-import { sum } from './sum'
+import { EventListener } from './EventListener'
 
-console.log(sum(1,2))
+class Application {
+  start() {
+    const eventListener = new EventListener()
+    const button = document.getElementById('deleteAllDoneTask')
+
+    // HTMLElementが返らない場合があるため
+    if (!button) return
+
+    console.log(button)
+
+    eventListener.add(
+      'sample',
+      'click',
+      button,
+      () => alert('clicked'),
+    )
+  }
+}
+
+
+
+window.addEventListener('load', () => {
+  const app = new Application()
+  app.start()
+})
