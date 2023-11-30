84299ed1d7ffe1c52671702d4a77ae2ebe682e58
nishi633 <citron.nn633@gmail,com>
2023-11-30 22:03:33 +0900

    Can delete task on each status

--- browser-app/ts/TaskRenderer.ts
+++ browser-app/ts/TaskRenderer.ts
@@ -22,7 +22,17 @@ export class TaskRenderer {
 
     if (!taskEl) return
 
-    this.todoList.removeChild(taskEl)
+    if (task.status === statusMap.todo) {
+      this.todoList.removeChild(taskEl)
+    }
+
+    if (task.status === statusMap.doing) {
+      this.doingList.removeChild(taskEl)
+    }
+
+    if (task.status === statusMap.done) {
+      this.doneList.removeChild(taskEl)
+    }
   }
 
   subscribeDragAndDrop(onDrop: (el: Element, sibling: Element | null, newStatus: Status) => void) {
