c637d33934fcaad818817b6b6cfb44e6e04a6bf1
nishi633 <citron.nn633@gmail,com>
2023-12-14 23:27:52 +0900

    Add Button components

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,15 @@
 import { Text } from './libs/Text'
 import { Heading } from './libs/Heading'
+import { Button } from './libs/Button'
 
 export const App = () => {
   return (
     <>
       <Text text='Hello, World!' />
       <Heading tag='h1'>見出し</Heading>
+      <Button onClick={() => console.log('clicked!')} title='Button' type='primary' width={96} />
+      <Button onClick={() => console.warn('clicked!')} title='Button' type='secondary' />
+      <Button onClick={() => console.error('clicked!')} title='Button' type='error' />
       <Heading tag='h1'>
         <span>hellow, world!</span>
       </Heading>
